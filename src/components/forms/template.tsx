9ea78b438a84d07f98cacd1dcb553179ac10fde6
Eren Altan <erenaltan@gmail.com>
2024-01-03 08:59:13 +0000

    Update neon shadow effect on tech cards with red to purple gradient and reduced blur

--- src/components/forms/template.tsx
+++ src/components/forms/template.tsx
@@ -22,8 +22,7 @@ export default function Template({ title, logo, form }: Props) {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Card className="group relative flex h-[150px] w-[150px] justify-center p-5">
-          <div className="absolute inset-0 rounded-lg bg-white bg-opacity-50 opacity-0 shadow-lg transition-opacity duration-300 group-hover:opacity-100 group-hover:shadow-2xl group-hover:shadow-purple-500"></div>
+        <Card className="neon-shadow group relative my-4 flex h-[150px] w-[150px] justify-center p-5">
           <CardContent className="transform transition-transform duration-300 group-hover:scale-110">
             {logo}
           </CardContent>
